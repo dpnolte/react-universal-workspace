09f952dfbe849ee98406ee10cb4f478329a59a90
Derk <dpnolte@gmail.com>
2019-05-21 16:28:33 +0200

    fixed unused Platform for tsc

--- packages/ui/src/elements/Banner.tsx
+++ packages/ui/src/elements/Banner.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, StyleSheet, Platform } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { ReactIcon } from '../ReactIcon'
 import { H1 } from './H1'
 import { isDom } from '../platforms'
